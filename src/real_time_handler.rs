fc92e17842bf19de14c5e35fecc830bbdeab4201
Nighttrek <daniels@dual4t.com>
2023-04-19 06:37:28 -0700

    semi tested update to support session IDs sent to the client. Not tested since im off to bed

--- src/real_time_handler.rs
+++ src/real_time_handler.rs
@@ -29,6 +29,12 @@ impl Default for ClientMessage {
         }
     }
 }
+
+#[derive(Serialize, Debug)]
+pub struct ClientSessionStartMessage {
+    pub session_id: usize,
+}
+
 #[derive(Serialize, Debug)]
 pub struct Position {
     pub x: i32,
@@ -58,6 +64,7 @@ impl Actor for SessionsStorage {
 }
 
 struct MyWs {
+    session_id: usize,
     sessions_storage: Data<Arc<Mutex<SessionsStorage>>>,
 }
 
@@ -66,19 +73,19 @@ impl Actor for MyWs {
 }
 
 // function which takes a ws::Message::text and mutable Sef::Context and returns a ws::Message::text
-fn handle_position_update(text: ByteString, session_storage: Data<Arc<Mutex<SessionsStorage>>>) -> String {
+fn handle_position_update(text: ByteString, session_id: usize, session_storage: Data<Arc<Mutex<SessionsStorage>>>) -> String {
     let input_string = String::from(text);
     // convert the JSON string into a clientMessage struct
     let client_message: ClientMessage = serde_json::from_str(&input_string).unwrap_or_default();
     let mut all_sessions = session_storage.lock().unwrap(); // need beetter error handling here TODO ERROR HANDLING URGENT
 
     // check the length of the positions Vector and if its less than 10, push the new position
-    if all_sessions.sessions[client_message.session_id].positions.len() < 10 {
-        all_sessions.sessions[client_message.session_id].positions.push(Position { x: client_message.x, y: client_message.y });
+    if all_sessions.sessions[session_id].positions.len() < 10 {
+        all_sessions.sessions[session_id].positions.push(Position { x: client_message.x, y: client_message.y });
     } else {
         // if the length is 10, remove the first element and push the new position
-        all_sessions.sessions[client_message.session_id].positions.remove(0);
-        all_sessions.sessions[client_message.session_id].positions.push(Position { x: client_message.x, y: client_message.y });
+        all_sessions.sessions[session_id].positions.remove(0);
+        all_sessions.sessions[session_id].positions.push(Position { x: client_message.x, y: client_message.y });
     }
     // update the session storage
     // all_sessions.sessions[client_message.session_id].positions.push(Position { x: client_message.x, y: client_message.y });
@@ -88,6 +95,11 @@ fn handle_position_update(text: ByteString, session_storage: Data<Arc<Mutex<Sess
     return json_bytes;
 }
 
+fn handle_session_start( session_id: usize) -> String {
+    let client_session_start_message = ClientSessionStartMessage { session_id: session_id };
+    let json_string = serde_json::to_string(&client_session_start_message).unwrap_or_default();
+    return json_string;
+}
 
 /// Handler for ws::Message message
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
@@ -96,17 +108,30 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
             //handle updating the position Array
             Ok(ws::Message::Text(text)) => {
                 println!("Server got message: {}", text);
-                let json_string = handle_position_update(text, self.sessions_storage.clone());
-                ctx.text(json_string);
+                if text == "Hello Server!" {
+                    handle_session_start(self.session_id);
+                    ctx.text("{session_id: {}");
+                    
+                } else {
+                    let json_string = handle_position_update(text, self.session_id, self.sessions_storage.clone());
+                    ctx.text(json_string);
+                }
              } 
             _ => (),
         }
     }
-    
 }
 
 pub async fn ws_handler(req: HttpRequest, stream: Payload, data: Data<Arc<Mutex<SessionsStorage>>>) -> Result<HttpResponse, Error> {
-    let resp = ws::start(MyWs {sessions_storage: data}, &req, stream);
+    let mut mutable_data = data.lock().unwrap();
+    let last_session_id = mutable_data.sessions.len() - 1;
+    if last_session_id >= 100 {
+        mutable_data.sessions.remove(0);
+    }
+    mutable_data.sessions.push(Session { positions: vec![Position { x: 0, y: 0 }] }); // make sure to add a new session whenever we create one.
+    drop(mutable_data); // required to drop the value here
+    println!("last_session_id: {}", last_session_id);
+    let resp = ws::start(MyWs {session_id:last_session_id, sessions_storage: data}, &req, stream);
     println!("{:?}", resp);
     resp
 }
